b2587c666546498af01f466e76d62805db341d98
Braden Helmer <bradenhelmeraus@gmail.com>
2022-11-19 10:43:19 -0500

    email fetching working

--- README.md
+++ README.md
@@ -7,9 +7,3 @@ Login: csc424projectdummy@gmail.com <br>
 Password: computersecurity <br>
 
 We used the [SalemWebNetwork](https://preferences.salemwebnetwork.com/) to generate a bunch of spam email.
-
-## Python Setup
-Using Pipenv for dependency management, when cloning repo: <br>
-1. Run ```pipenv install --dev``` to setup virtual environment.
-2. Run ```pipenv shell``` to spawn shell in venv.<br>
-When editing the ```Pipfile```, make sure to run the command in step 1 again to regenerate ```Pipfile.lock``` to be committed.
--- email_fetcher.py
+++ email_fetcher.py
@@ -2,6 +2,49 @@
 # ~~~~~~~~~~~~~~~~
 # This file fetches all the emails from the user for checking
 import email
-import imaplib
+from imaplib import IMAP4_SSL
+
+DUMMY_EMAIL_ADDRESS = "csc424projectdummy@gmail.com"
+# This is an app specific password for IMAP access, it differs from the actual Google account password.
+PASSWORD = "mpuietxubasitdja"
+IMAP_ADDRESS = "imap.gmail.com"
+
+def get_body(message):
+    if message.is_multipart():
+        return (
+            get_body(message.get_payload(0))
+            if message.is_multipart()
+            else message.get_payload(None, True)
+        )
 
 
+def search(key, value, con):
+    # value is the email address we are searching emails from
+    data = con.search(None, key, '"{}"'.format(value))
+    return data
+
+
+def get_emails(result_bytes, con):
+    print(result_bytes)
+    messages = []
+    for num in result_bytes[1][0].split():
+        data = con.fetch(num, "(INSERT PARSE OPTION HERE)")
+        messages.append(data)
+    return messages
+
+
+def login_and_retrieve():
+    con = IMAP4_SSL(IMAP_ADDRESS)
+    con.login(DUMMY_EMAIL_ADDRESS, PASSWORD)
+    con.select("Inbox")
+    return get_emails(search("FROM", "INSERT_EMAIL_ADDRESS_HERE", con), con)
+
+
+def main():
+    emails = login_and_retrieve()
+    for email in emails:
+        print(email)
+
+
+if __name__ == "__main__":
+    main()
